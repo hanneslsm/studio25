c3c35e10be06974c42ba87641b068d1e3d7f7989
hanneslsm <HannesLeismann@gmail.com>
2024-09-20 15:17:46 +0200

    Change path to editor-style.css

--- inc/setup.php
+++ inc/setup.php
@@ -2,7 +2,7 @@
 
 /**
  * Setup
- * 
+ *
  * @package prolooks
  * @since 0.1
  * @link https://developer.wordpress.org/themes/block-themes/block-theme-setup/
@@ -11,13 +11,13 @@
 
 
 if (!function_exists('prolooks_setup')) :
-    function prolooks_setup()
-    {
-        // Make theme available for translation.
-        load_theme_textdomain('prolooks', get_template_directory() . '/languages');
+	function prolooks_setup()
+	{
+		// Make theme available for translation.
+		load_theme_textdomain('prolooks', get_template_directory() . '/languages');
 
-        // Enqueue editor styles.
-        add_editor_style('editor-style.css');
-    }
+		// Enqueue editor styles.
+		add_editor_style('assets/css/editor-style.css');
+	}
 endif; // prolooks_setup
 add_action('after_setup_theme', 'prolooks_setup');
