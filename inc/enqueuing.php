6f53be65a7dd53358e580eb3e034aed452b39ea8
hanneslsm <HannesLeismann@gmail.com>
2024-10-14 19:22:31 +0200

    Update enquing to `enqueue_block_assets`

--- inc/enqueuing.php
+++ inc/enqueuing.php
@@ -10,25 +10,24 @@
 /**
  * Enqueue assets for both frontend and block editor.
  */
-function studio25_enqueue_assets() {
+function studio25_enqueue_block_assets() {
     $asset_file = include( get_template_directory() . '/public/main.asset.php' );
 
-    // Enqueue script
+    // Enqueue frontend and block editor style
+    wp_enqueue_style(
+        'studio25-block-styles',
+        get_template_directory_uri() . '/public/main.css',
+        array(),
+        $asset_file['version']
+    );
+
+    // Enqueue frontend and block editor script
     wp_enqueue_script(
-        'studio25-scripts',
+        'studio25-block-scripts',
         get_template_directory_uri() . '/public/main.js',
         $asset_file['dependencies'],
         $asset_file['version'],
         true  // Load in footer
     );
-
-    // Enqueue style
-    wp_enqueue_style(
-        'studio25-styles',
-        get_template_directory_uri() . '/public/main.css',
-        array(),
-        $asset_file['version']
-    );
 }
-add_action( 'wp_enqueue_scripts', 'studio25_enqueue_assets' );
-add_action( 'enqueue_block_editor_assets', 'studio25_enqueue_assets' );
+add_action( 'enqueue_block_assets', 'studio25_enqueue_block_assets' );
