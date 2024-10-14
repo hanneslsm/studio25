ccd80e9a1ee037c41384dbbd91761e77d21431ff
hanneslsm <HannesLeismann@gmail.com>
2024-10-14 16:56:24 +0200

    Change folder name from build to public

--- inc/enqueuing.php
+++ inc/enqueuing.php
@@ -7,28 +7,28 @@
  * @since 0.1
  */
 
-
-
-
 /**
- * Enqueue Editor assets.
+ * Enqueue assets for both frontend and block editor.
  */
-function studio25_enqueue_editor_assets()
-{
-	$asset_file = include(get_template_directory() . '/build/index.asset.php');
+function studio25_enqueue_assets() {
+    $asset_file = include( get_template_directory() . '/public/main.asset.php' );
 
-	wp_enqueue_script(
-		'studio25-editor-scripts',
-		get_template_directory_uri() . '/build/index.js',
-		$asset_file['dependencies'],
-		$asset_file['version']
-	);
+    // Enqueue script
+    wp_enqueue_script(
+        'studio25-scripts',
+        get_template_directory_uri() . '/public/main.js',
+        $asset_file['dependencies'],
+        $asset_file['version'],
+        true  // Load in footer
+    );
 
-	wp_enqueue_style(
-		'studio25-editor-styles',
-		get_template_directory_uri() . '/build/index.css',
-		array(),
-		$asset_file['version']
-	);
+    // Enqueue style
+    wp_enqueue_style(
+        'studio25-styles',
+        get_template_directory_uri() . '/public/main.css',
+        array(),
+        $asset_file['version']
+    );
 }
-add_action('enqueue_block_editor_assets', 'studio25_enqueue_editor_assets');
+add_action( 'wp_enqueue_scripts', 'studio25_enqueue_assets' );
+add_action( 'enqueue_block_editor_assets', 'studio25_enqueue_assets' );
