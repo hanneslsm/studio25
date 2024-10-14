7bda396dc3c64062afddc1edd7f35ca4a5305aeb
hanneslsm <HannesLeismann@gmail.com>
2024-10-14 20:30:23 +0200

    Add check

--- resources/blocks/core-group/script.js
+++ resources/blocks/core-group/script.js
@@ -1,18 +1,25 @@
 /** Spotlight Effect */
+import { registerBlockVariation } from '@wordpress/blocks';
+
 
 document.addEventListener('DOMContentLoaded', function() {
-const gradient = document.querySelector('.is-style-group-spotlight');
+    const gradient = document.querySelector('.is-style-group-spotlight');
 
+    // Ensure the element exists before adding event listeners and manipulating styles
+    if (gradient) {
+        function onMouseMove(event) {
+            gradient.style.backgroundImage =
+                'radial-gradient(at ' +
+                event.clientX +
+                'px ' +
+                event.clientY +
+                'px, rgba(24, 93, 106, 0) 0, #1F3A47 70%)';
+        }
 
-function onMouseMove(event) {
-	gradient.style.backgroundImage =
-		'radial-gradient(at ' +
-		event.clientX +
-		'px ' +
-		event.clientY +
-		'px, rgba(24, 93, 106, 0) 0, #1F3A47 70%)';
-}
-document.addEventListener('mousemove', onMouseMove);
+        // Add the mousemove event listener only if the element exists
+        document.addEventListener('mousemove', onMouseMove);
+    }
 });
+
 
 console.log('Hello Script!');
