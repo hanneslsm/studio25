48f65bc8f92ac6260b72e58d1f0ab9198fbb6683
hanneslsm <HannesLeismann@gmail.com>
2024-10-15 16:36:46 +0200

    Update

--- inc/enqueuing.php
+++ inc/enqueuing.php
@@ -38,12 +38,8 @@ add_action( 'enqueue_block_assets', 'studio25_enqueue_block_assets' );
 
 // wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
 function theme_gsap_script(){
-    // The core GSAP library
     wp_enqueue_script( 'gsap-js', 'https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js', array(), false, true );
-    // ScrollTrigger - with gsap.js passed as a dependency
     wp_enqueue_script( 'gsap-st', 'https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/ScrollTrigger.min.js', array('gsap-js'), false, true );
-    // Your animation code file - with gsap.js passed as a dependency
-    wp_enqueue_script( 'gsap-js2', get_template_directory_uri() . 'js/app.js', array('gsap-js'), false, true );
 }
 
 add_action( 'wp_enqueue_scripts', 'theme_gsap_script' );
