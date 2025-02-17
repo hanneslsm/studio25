5fadfd880c595dc996ab5320c1672ff41949dc7f
hanneslsm <HannesLeismann@gmail.com>
2025-02-17 17:18:45 +0100

    Update inc files

--- inc/enqueuing.php
+++ inc/enqueuing.php
@@ -1,35 +1,100 @@
 <?php
-
 /**
- * Enqueue files
+ * Enqueue frontend and editor styles
  *
- * @package prolooks
- * @since 0.1
+ * @package studio25
+ * @since 0.1.0
  */
 
 /**
- * Enqueue assets for both frontend and block editor.
- */
-function studio25_enqueue_block_assets() {
-    $asset_file = include( get_template_directory() . '/public/main.asset.php' );
+* global.css and global.js are loaded for both frontend and editor.
+* screen.css is only loaded on the frontend.
+* editor.css is exclusively loaded in the block editor.
+*/
 
-    // Enqueue frontend and block editor style
+
+function studio25_enqueue_scripts() {
+    // Enqueue the global CSS for both the frontend and editor
+    $global_style_path = get_template_directory_uri() . '/build/css/global.css';
+    $global_style_asset = require get_template_directory() . '/build/css/global.asset.php';
+
     wp_enqueue_style(
-        'studio25-block-styles',
-        get_template_directory_uri() . '/public/main.css',
-        array(),
-        $asset_file['version']
+        'studio25-global-style',
+        $global_style_path,
+        $global_style_asset['dependencies'],
+        $global_style_asset['version']
     );
 
-    // Enqueue frontend and block editor script
+    // Enqueue the global JavaScript for both the frontend and editor
+    $global_script_path = get_template_directory_uri() . '/build/js/global.js';
+    $global_script_asset = require get_template_directory() . '/build/js/global.asset.php';
+
     wp_enqueue_script(
-        'studio25-block-scripts',
-        get_template_directory_uri() . '/public/main.js',
-        $asset_file['dependencies'],
-        $asset_file['version'],
-        true  // Load in footer
+        'studio25-global-script',
+        $global_script_path,
+        $global_script_asset['dependencies'],
+        $global_script_asset['version'],
+        true // Load in the footer
     );
 }
-add_action( 'enqueue_block_assets', 'studio25_enqueue_block_assets' );
+add_action('enqueue_block_assets', 'studio25_enqueue_scripts');
 
+function studio25_enqueue_frontend_styles() {
+    // Enqueue the screen CSS only on the frontend
+    $screen_style_path = get_template_directory_uri() . '/build/css/screen.css';
+    $screen_style_asset = require get_template_directory() . '/build/css/screen.asset.php';
+
+    wp_enqueue_style(
+        'studio25-screen-style',
+        $screen_style_path,
+        $screen_style_asset['dependencies'],
+        $screen_style_asset['version']
+    );
+}
+add_action('wp_enqueue_scripts', 'studio25_enqueue_frontend_styles');
+
+function studio25_enqueue_editor_styles() {
+    // Enqueue the editor CSS only in the block editor
+    $editor_style_path = get_template_directory_uri() . '/build/css/editor.css';
+    $editor_style_asset = require get_template_directory() . '/build/css/editor.asset.php';
+
+    wp_enqueue_style(
+        'studio25-editor-style',
+        $editor_style_path,
+        $editor_style_asset['dependencies'],
+        $editor_style_asset['version']
+    );
+}
+add_action('enqueue_block_editor_assets', 'studio25_enqueue_editor_styles');
+
+
+function studio25_enqueue_block_styles() {
+    // Define the directory containing block styles
+    $blocks_dir = get_theme_file_path( 'build/css/blocks/' );
+
+    // Use glob() to get all .css files in the blocks directory
+    $block_styles = glob( $blocks_dir . '*.css' );
+
+    // Iterate over each stylesheet
+    foreach ( $block_styles as $style_path ) {
+        // Extract the block name from the filename
+        $filename = basename( $style_path, '.css' );
+
+        // Replace only the first hyphen with a slash
+        $block_name = preg_replace( '/-/', '/', $filename, 1 );
+
+        // Construct the URL for the stylesheet
+        $style_uri = get_theme_file_uri( 'build/css/blocks/' . $filename . '.css' );
+
+        // Enqueue the block style
+        wp_enqueue_block_style(
+            $block_name,
+            [
+                'handle' => 'studio25-' . $filename . '-style',
+                'src'    => $style_uri,
+            ]
+        );
+    }
+}
+add_action( 'init', 'studio25_enqueue_block_styles' );
 
--- inc/setup.php
+++ inc/setup.php
@@ -3,21 +3,21 @@
 /**
  * Setup
  *
- * @package prolooks
- * @since 0.1
+ * @package studio25
+ * @since 0.1.0
  * @link https://developer.wordpress.org/themes/block-themes/block-theme-setup/
  */
 
 
 
-if (!function_exists('prolooks_setup')) :
-	function prolooks_setup()
+if (!function_exists('studio25_setup')) :
+	function studio25_setup()
 	{
 		// Make theme available for translation.
-		load_theme_textdomain('prolooks', get_template_directory() . '/languages');
+		load_theme_textdomain('studio25', get_template_directory() . '/languages');
 
 		// Enqueue editor styles.
 		add_editor_style('assets/css/editor-style.css');
 	}
-endif; // prolooks_setup
-add_action('after_setup_theme', 'prolooks_setup');
+endif; // studio25_setup
+add_action('after_setup_theme', 'studio25_setup');
