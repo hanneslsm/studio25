69c43c442ac2a66681f2d0565621bd279cacd6e9
hanneslsm <HannesLeismann@gmail.com>
2025-02-17 18:22:11 +0100

    Add src files

---
+++ src/global.js
@@ -0,0 +1,9 @@
+/**
+ * global.js is loaded for both frontend and editor.
+ */
+
+/* Import Custom Block Styles & Block Variations */
+import './block-styles.js';
+import './block-variations.js';
+
+import './js/utilities/on-stuck.js';
